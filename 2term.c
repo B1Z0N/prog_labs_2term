080374672ca2f11965dac11fe09f58e525c64075
Nick Fedurko <koliafedurko22@gmail.com>
2018-03-13 10:32:24 +0200

    Added 5 lab and refactored header and source code

--- 2term.c
+++ 2term.c
@@ -22,61 +22,9 @@ int main() {
 
     return (0);
 }
-unote *init_unote() {
-    unote *temp = (unote *) malloc(sizeof(unote));
+//first lab
 
-    return(temp);
-}
-int read_data(unote *un[]) {
-    FILE* f;
-    int length = -1;
-    if((f = fopen("2ndfac.txt", "r")) == NULL) {
-        fprintf(stderr,"Error reading 1st file");
-        exit(-1);
-    }
-    while(!feof(f) && length < LAB2) {
-        length++;
-        fgets(un[length]->fac, LAB2, f);
-    }
-    fclose(f);
 
-    if((f = fopen("2ndspec.txt", "r")) == NULL) {
-        fprintf(stderr,"Error reading 2nd file");
-        exit(-1);
-    }
-    int i = -1;
-    while(!feof(f) && i < length) {
-        i++;
-        fgets(un[i]->spec, LAB2, f);
-    }
-    fclose(f);
-
-    return(length - 1);
-}
-void sort_un(unote * un[], int length) {
-    char temp1[LAB2];
-    int temp2;
-    for(int i = 0; i < length; i++) {
-        temp2 = i;
-        for(int j = i + 1; j <= length; j++)
-            if(strcmp(un[temp2]->fac, un[j]->fac) > 0) 
-                temp2 = j; 
-        
-        strcpy(temp1, un[temp2]->fac);
-        strcpy(un[temp2]->fac, un[i]->fac);
-        strcpy(un[i]->fac, temp1);
-
-        strcpy(temp1, un[temp2]->spec);
-        strcpy(un[temp2]->spec, un[i]->spec);
-        strcpy(un[i]->spec, temp1);
-    }
-}
-void show_un(unote * un[], int length) {
-    for(int i = 0; i <= length; i++) {
-        printf("%d|%s", i + 1, un[i]->fac);
-        printf("    %s\n", un[i]->spec);
-    }
-}
 void lab1() {
     FILE *f;
     int i = 0;
@@ -132,12 +80,138 @@ void lab2() {
     for(int s = 0; s <= i; s++)
         free(un[s]);
   }
+
+//second lab
+
+unote *init_unote() {
+    unote *temp = (unote *) malloc(sizeof(unote));
+
+    return(temp);
+}
+int read_data(unote *un[]) {
+    FILE* f;
+    int length = -1;
+    if((f = fopen("2ndfac.txt", "r")) == NULL) {
+        fprintf(stderr,"Error reading 1st file");
+        exit(-1);
+    }
+    while(!feof(f) && length < LAB2) {
+        length++;
+        fgets(un[length]->fac, LAB2, f);
+    }
+    fclose(f);
+
+    if((f = fopen("2ndspec.txt", "r")) == NULL) {
+        fprintf(stderr,"Error reading 2nd file");
+        exit(-1);
+    }
+    int i = -1;
+    while(!feof(f) && i < length) {
+        i++;
+        fgets(un[i]->spec, LAB2, f);
+    }
+    fclose(f);
+
+    return(length - 1);
+}
+void sort_un(unote * un[], int length) {
+    char temp1[LAB2];
+    int temp2;
+    for(int i = 0; i < length; i++) {
+        temp2 = i;
+        for(int j = i + 1; j <= length; j++)
+            if(strcmp(un[temp2]->fac, un[j]->fac) > 0) 
+                temp2 = j; 
+        
+        strcpy(temp1, un[temp2]->fac);
+        strcpy(un[temp2]->fac, un[i]->fac);
+        strcpy(un[i]->fac, temp1);
+
+        strcpy(temp1, un[temp2]->spec);
+        strcpy(un[temp2]->spec, un[i]->spec);
+        strcpy(un[i]->spec, temp1);
+    }
+}
+void show_un(unote * un[], int length) {
+    for(int i = 0; i <= length; i++) {
+        printf("%d|%s", i + 1, un[i]->fac);
+        printf("    %s\n", un[i]->spec);
+    }
+}
+
+//third lab
+
 void lab3() {
 
 }
+
+//fourth lab
+
 void lab4() {
 
 }
-void lab5() {
 
+//fifth lab
+
+void lab5() {
+    int key;
+    printf("Enter first key:");
+    scanf("%d", &key);
+    node * root = init_node(key);
+    
+    printf("Enter new key, to stop(-1)");
+    scanf("%d", &key);
+    while(key != -1) {
+        add_node(key, &root);
+        scanf("%d", &key);
+        printf("Enter new key, to stop(-1)");
+    }
+
+    printf("Enter key, which number you`d like to know: ");
+    scanf("%d", &key);
+    printf("Number of node with key %d:   %d\n", key, search_node(root, key));
+    printf("List length:  %d\n", list_len(root));
 }
+
+node * init_node(int key) {
+    node * temp = (node *) malloc(sizeof(node));
+    temp->next = NULL;
+    temp->key = key;
+}
+void add_node(int key, node ** root) {
+    node * temp = *root;
+    
+    while(temp->next)
+        temp = temp->next;
+    
+    node * next = (node *) malloc(sizeof(node));
+    
+    temp->next = next;
+    next->next = NULL;
+    next->key = key;
+}
+int search_node(node * root, int key) {
+    node * temp = root;
+    int i = 1;
+    while(temp->key != key) {
+        temp = temp->next;
+        i++;
+        if(temp == NULL) { i = 0; break; }
+    }
+
+    return(i);
+}
+int list_len(node * root) {
+    node * temp = root;
+    int i = 1;
+    while(temp->next != NULL) {
+        temp = temp->next;
+        i++;
+    }
+
+    return(i);
+}
+
+//end
+
+
--- 2term.h
+++ 2term.h
@@ -2,9 +2,18 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+//first lab
+
 #define LAB1 80
+
+void lab1();
+
+//second lab
+
 #define LAB2 40
-//for second lab
+
+void lab2();
+
 typedef struct university_note {
     char fac[LAB2];
     char spec[LAB2];
@@ -15,8 +24,27 @@ void show_un(unote * un[], int length);
 void sort_un(unote * un[], int length);
 int read_data(unote * un[]);
 
-void lab1();
-void lab2();
+//third lab
+
 void lab3();
+
+//fourth lab
+
 void lab4();
+
+//fifth lab
+
 void lab5();
+
+typedef struct node {
+    struct node * next;
+    int key;
+} node;
+
+node * init_node(int key);
+void add_node(int key, node ** root);
+int search_node(node * root, int key);
+int list_len(node * root);
+
+//end
+
